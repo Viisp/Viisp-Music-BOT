7c7144a01099e773a02904d509ec3b371fe6d6ea
Viisp <viisp@viisp.com>
2026-04-18 16:31:46 +0200

    fix: prepend ffmpeg-static dir to PATH for DisTube ffmpeg detection

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,10 @@ const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { DisTube } = require('distube');
 const { SpotifyPlugin } = require('@distube/spotify');
 const { YtDlpPlugin } = require('@distube/yt-dlp');
-// Point DisTube to the bundled ffmpeg-static binary
-process.env.FFMPEG_PATH = require('ffmpeg-static');
+// Add ffmpeg-static binary directory to PATH so DisTube can find 'ffmpeg'
+const ffmpegStatic = require('ffmpeg-static');
+const nodePath = require('path');
+process.env.PATH = nodePath.dirname(ffmpegStatic) + ':' + (process.env.PATH || '');
 
 const client = new Client({
   intents: [
